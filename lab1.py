8850ba22f30a1330c183277725201950aab3c030
NNursat <salau1977@gmial.com>
2025-01-24 05:54:52 +0500

    lab 1 almost

--- lab1.py
+++ lab1.py
@@ -7,3 +7,49 @@ if 5 > 2:
 x = 10
 y = "word"
 
+#comment
+"""
+multiple
+line
+comment
+"""
+
+print(x)
+print(y)
+
+x = str(3)
+y = float(3)
+
+print(type(x))
+print(type(y))
+x1, y1, z1 = 1, 2, 3
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+def newFunc():
+    print("useless function")
+
+newFunc()
+
+a = "hello Mr.Abeshov"
+
+print(a[5])
+
+for x in a:
+    print(x)
+
+def lenfunc(str):
+    print(len(str))
+
+lenfunc(a)
+
+if "Mr" not in a:
+    print("yes")
+else:
+    print("NO")
+
+print(a[:15])
